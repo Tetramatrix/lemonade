e50a51f8adc0d85c35af95fafdf0d4afde4fe865
Tetramatrix <chibox@gmail.com>
2025-11-11 09:46:45 +0100

    update init scripts

--- src/cpp/lemonade-beta.bat
+++ src/cpp/lemonade-beta.bat
@@ -1,11 +1,11 @@
 @echo off
 
 REM Change to parent directory
-pushd "%~dp0.."
+pushd "%~dp0"
 
 echo Starting Lemonade Server (ROCm)...
 
-powershell.exe -Command "lemonade-server-beta serve --llamacpp rocm --ctx-size 128000"
+powershell.exe -Command ".\lemonade-server-beta serve --llamacpp rocm --ctx-size 128000"
 
 echo Server process has finished or was stopped.
 pause
